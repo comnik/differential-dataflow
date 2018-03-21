30c47faa94f4e176e353533912bfd53ec546f945
niko <me@nikolasgoebel.com>
2018-03-21 15:27:24 +0100

    [WIP] First datalog computation

--- src/lib.rs
+++ src/lib.rs
@@ -109,27 +109,67 @@ pub mod collection;
 // JS INTEROP
 //
 
-#[macro_use]
-extern crate stdweb;
-#[macro_use]
-extern crate lazy_static;
+#[macro_use] extern crate stdweb;
+#[macro_use] extern crate serde_derive;
 
 use stdweb::js_export;
 
 use std::hash::Hash;
+use std::cmp::Ordering;
 
 use lattice::Lattice;
 use timely::{Root, Allocator};
 use timely::dataflow::*;
+use timely::dataflow::scopes::Child;
 use timely::dataflow::operators::*;
-use timely::dataflow::operators::input::Handle;
 use timely::dataflow::channels::pact::Pipeline;
 use timely::execute::{setup_threadless};
 
-const ATTR_EVENT_TYPE: u32 = 0;
+use input::{Input, InputSession};
+use operators::arrange::{ArrangeByKey, ArrangeBySelf};
+use operators::join::JoinCore;
 
-type Datom = u32;//(u64, u32, u64);
-type InputHandle = Handle<u64, Datom>;
+const ATTR_NAME: u32 = 100;
+const ATTR_FRIEND: u32 = 200;
+
+const VALUE_NAME_1: u64 = 100;
+const VALUE_NAME_2: u64 = 101;
+
+// #[derive(Eq, Clone, Serialize, Deserialize, Abomonation, Debug)]
+#[derive(Serialize, Deserialize)]
+pub struct JsDatom {
+    e: u64,
+    a: u32,
+    v: u64,
+}
+
+type Datom = (u64, u32, u64);
+
+js_serializable!(JsDatom);
+js_deserializable!(JsDatom);
+
+// impl Ord for Datom {
+//     fn cmp(&self, other: &Datom) -> Ordering {
+//         self.e.cmp(&other.e)
+//             .then(self.a.cmp(&other.a))
+//             .then(self.v.cmp(&other.v))
+//     }
+// }
+
+// impl PartialOrd for Datom {
+//     fn partial_cmp(&self, other: &Datom) -> Option<Ordering> {
+//         Some(self.cmp(other))
+//     }
+// }
+
+// impl PartialEq for Datom {
+//     fn eq(&self, other: &Datom) -> bool {
+//         self.e == other.e
+//     }
+// }
+
+type InputHandle = InputSession<u64, Datom, isize>;
+
 
 pub struct Context {
     root: Root<Allocator>,
@@ -161,9 +201,9 @@ pub fn register(computation: u32) -> bool {
         match CTX {
             None => false,
             Some(ref mut ctx) => {
-                let mut input = ctx.root.dataflow::<u64,_,_>(|scope| {
-                    let (datoms_in, datoms) = scope.new_input();
-                    interpret(datoms, computation);
+                let mut input = ctx.root.dataflow::<u64,_,_>(|mut scope| {
+                    let (datoms_in, datoms) = scope.new_collection::<>();
+                    interpret::<Child<_, _>>(&mut scope, &datoms, computation);
                     
                     datoms_in
                 });
@@ -176,53 +216,73 @@ pub fn register(computation: u32) -> bool {
     }
 }
 
+// struct Relation {
+    
+// }
+
 /// This takes a potentially JS-generated description of a dataflow
 /// graph and turns it into one that differential can understand.
-fn interpret<G: Scope> (datoms: Stream<G, Datom>, computation: u32) where G::Timestamp: Lattice+Hash+Ord {
+fn interpret<'a, G: Scope+Input<'a, Allocator, u64>> (scope: &mut G,
+                                                      datoms: &Collection<G, Datom>,
+                                                      computation: u32) where G::Timestamp: Lattice+Hash+Ord {
 
-    match computation {
-        0 => {
-            // [_ :event/type _]
-            let stream = datoms
-                .map(|x| x + 1)
-                .sink(Pipeline, "example", |input| {
-                    while let Some((time, data)) = input.next() {
-                        for datum in data.iter() {
-                            js! {
-                                var datum = @{datum};
-                                __UGLY_DIFF_HOOK(datum);
-                            }
-                        }
-                    }
-                });
+    // let eav = datoms.map(|(e, a, v)| (e, (a, v))).arrange_by_key();
+    let aev = datoms.map(|(e, a, v)| (a, (e, v))).arrange_by_key();
 
-            // results
-            //     .filter(move |&d| d == ATTR_EVENT_TYPE);
-                // .filter(move |&(e, a, v)| a == ATTR_EVENT_TYPE);
-        }
-        1 => {
-            // [_ :event/type 400]
-            let stream = datoms
-                .map(|x| x - 1)
-                .sink(Pipeline, "example", |input| {
-                    while let Some((time, data)) = input.next() {
-                        for datum in data.iter() {
-                            js! {
-                                var datum = @{datum};
-                                __UGLY_DIFF_HOOK(datum);
-                            }
-                        }
-                    }
-                });
+    let in1 = scope
+        .new_collection_from(vec![ATTR_NAME]).1
+        .arrange_by_self();
 
-            // results
-            //     .filter(move |&(e, a, v)| a == ATTR_EVENT_TYPE)
-            //     .filter(move |&(e, a, v)| v == 400);
-        }
-        _ => {
-            panic!("error: Unknown computation");
-        }
-    };
+    let r1 = aev
+        .join_core(&aev, |a, &(e, v), _d2| Some((e, *a, v)))
+        .inspect(|res| {
+            let (e, a, v) = res.0;
+            js! {
+                var datom = @{JsDatom{e, a, v}};
+                __UGLY_DIFF_HOOK(datom);
+            }
+        });
+
+    // (join r1 r2)
+    // let r4 = r3
+    //     .map(|d| (d.e, d))
+    //     .join_map(&r1.map(|d| (d.e, d)), |e, &d1, &d2| Datom {e, a: d2.a, v: d2.v})
+    
+    // match computation {
+    //     0 => {
+    //         // [_ :event/type _]
+    //         let stream = datoms
+    //             .map(|d| Datom {v: d.v + 1, ..d} )
+    //             .sink(Pipeline, "example", |input| {
+    //                 while let Some((time, data)) = input.next() {
+    //                     for datom in data.iter() {
+    //                         js! {
+    //                             var datom = @{datom};
+    //                             __UGLY_DIFF_HOOK(datom);
+    //                         }
+    //                     }
+    //                 }
+    //             });
+    //     }
+    //     1 => {
+    //         // [_ :event/type 400]
+    //         let stream = datoms
+    //             .map(|d| Datom {v: d.v - 1, ..d} )
+    //             .sink(Pipeline, "example", |input| {
+    //                 while let Some((time, data)) = input.next() {
+    //                     for datom in data.iter() {
+    //                         js! {
+    //                             var datom = @{datom};
+    //                             __UGLY_DIFF_HOOK(datom);
+    //                         }
+    //                     }
+    //                 }
+    //             });
+    //     }
+    //     _ => {
+    //         panic!("error: Unknown computation");
+    //     }
+    // };
 }
 
 #[js_export]
@@ -248,18 +308,20 @@ pub fn demo() {
 }
 
 #[js_export]
-pub fn send(tx: u64, d: Vec<Datom>) -> bool {
-    let mut dmut = d.clone();
-    
+pub fn send(tx: u64, d: Vec<JsDatom>) -> bool {
     unsafe {
         match CTX {
             None => false,
             Some(ref mut ctx) => {
+
                 match ctx.input_handle {
                     None => false,
                     Some(ref mut input) => {
-                        input.send_batch(&mut dmut);
+                        for js_datom in d {
+                            input.insert((js_datom.e, js_datom.a, js_datom.v));
+                        }
                         input.advance_to(tx + 1);
+                        input.flush();
                         
                         ctx.root.step();
 
